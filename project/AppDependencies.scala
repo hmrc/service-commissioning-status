4865489b52c21ba0f411296ea0ec6ccb8a745b54
Shnick <1132919+Shnick@users.noreply.github.com>
2025-03-13 14:59:28 +0000

    BDOG-3401 add External to Tags enum

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,8 +5,8 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.10.0"
-  private val hmrcMongoVersion = "2.5.0"
+  private val bootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
